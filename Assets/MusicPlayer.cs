54fc20139b336fd48c22c5bebe12672b6a1227a3
Unknown <andrew_khang_le@hotmail.com>
2018-06-26 23:46:26 -0500

    Add splash screen and music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {	// Use this for initialization
+	void Start () {
+        Invoke("LoadFirstScene", 10f);
+	}
+
+    // Update is called once per frame
+    void LoadFirstScene() {
+        SceneManager.LoadScene(1);
+        DontDestroyOnLoad(this.gameObject);
+    }
+}

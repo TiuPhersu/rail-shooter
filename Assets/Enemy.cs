5fa60225f14b8e281421a27f44abcb2dfe21c3b5
Unknown <andrew_khang_le@hotmail.com>
2018-07-23 15:06:35 -0500

    Change script to add box collider to all future enemy ship

---
+++ Assets/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AddNonTriggerBoxCollider();
+	}
+
+    private void AddNonTriggerBoxCollider(){
+        //gameObject is the particular instance with the particular ship
+        //NonTrigger
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+        //Trigger
+        //gameObject.AddComponent<BoxCollider>();
+    }
+
+    void OnParticleCollision(GameObject other) {
+        //print("hit from particle" + gameObject.name);
+        Destroy(gameObject);
+    }
+}
